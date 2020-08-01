29ffdf9b271b55ecec48f1034a5b7cff585757e9
woodcol <fengmm521@gmail.com>
2020-08-01 12:06:34 +0800

    change python code

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-08-01 11:28:48
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os,sys
+
+
+def test():
+    a= {'a':1,'b':2}
+    print(a.a)
+#测试
+if __name__ == '__main__':
+    test()
